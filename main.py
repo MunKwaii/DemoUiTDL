81dde869bb64f22e49cbbd000d00f3682f1b03ca
QuocKhanh <Quockhanh6310@gmail.com>
2024-10-13 20:43:41 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from ui_interface import *
+from Custom_Widgets.Widgets import *
+
+settings = QSettings()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        #apply json style sheet
+        loadJsonStyle(self, self.ui)
+        
+        self.show()
+        
+#execute app
+if __name__ == "__main__":
+    app = QCoreApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
